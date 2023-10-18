ff6ffe18da466a6b9c9dac37928f01b55affd96d
Salma Lahrach <salmalahrach27@gmail.com>
2023-10-19 00:00:51 +0200

    Initial commit

---
+++ src/main/java/projectmanager/jdbc/SqlLiteSQLJDBC2.java
@@ -0,0 +1,45 @@
+package projectmanager.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SqlLiteSQLJDBC2 {
+
+    public static void main(String[] args) {
+        Connection c = null;
+        Statement stmt = null;
+        final String DBPath = "C:\\DEV\\SqLite\\sample.db";
+
+        try {
+            // Charge le driver JDBC de PostgreSQL
+            Class.forName("org.sqlite.JDBC");
+            
+            // Établit la connexion à la base de données
+            c = DriverManager
+               .getConnection("jdbc:sqlite:" + DBPath);
+            c.setAutoCommit(false);
+            System.out.println("Connexion réussie à la base de données!");
+
+            // Crée une instruction et exécute une requête
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Etudiant;");
+            
+            while (rs.next()) {
+                int id = rs.getInt("ID_Etudiant");
+                String nom = rs.getString("Nom");
+                System.out.println("ID = " + id);
+                System.out.println("NOM = " + nom);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opération réussie");
+    }
+}
