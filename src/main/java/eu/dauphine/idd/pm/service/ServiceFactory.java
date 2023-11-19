92c65e8952070c09cae148ed7c7416eb1c6a6840
salma <salmalahrach27@gmail.com>
2023-11-19 02:07:11 +0100

    Ajout de l'instance dans ServiceFactory

--- src/main/java/eu/dauphine/idd/pm/service/ServiceFactory.java
+++ src/main/java/eu/dauphine/idd/pm/service/ServiceFactory.java
@@ -1,16 +1,23 @@
 package eu.dauphine.idd.pm.service;
 
-import eu.dauphine.idd.pm.service.impl.FormationServiceImpl;
+import eu.dauphine.idd.pm.service.impl.*;
 
 public class ServiceFactory {
 
     private static FormationService formationService;
-    //private static EtudiantService etudiantService;
+    private static EtudiantService etudiantService;
 
     public static FormationService getFormationService() {
         if (formationService == null) {
             formationService = new FormationServiceImpl();
         }
         return formationService;
+    }
+    
+    public static EtudiantService getEtudiantService() {
+        if (etudiantService == null) {
+        	etudiantService = new EtudiantServiceImpl();
+        }
+        return etudiantService;
     }
 }
